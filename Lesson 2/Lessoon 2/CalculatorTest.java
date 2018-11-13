a2ff408894d7aee059fd965f23b03d8032caa718
unknown <Maxim Ivanov>
2018-11-13 20:59:58 +0300

    Улцчшенный калькулятор

---
+++ Lesson 2/Lessoon 2/CalculatorTest.java
@@ -0,0 +1,11 @@
+public class CalculatorTest {
+	
+	public static void main(String[] args) {
+		Calculator calc = new Calculator(); 
+		System.out.print(calc.num1);
+		System.out.print(calc.operation_value);
+		System.out.print(calc.num2);
+		System.out.print(" Ответ: "+  calc.outcome);
+	}
+}
+
