60eb59847f72159f2e836730f0c3752208b64d48
unknown <Maxim Ivanov>
2018-11-28 12:51:11 +0300

    Доделал калькулятор

---
+++ Lessons/Lesson 1/MyFirstGame.java
@@ -0,0 +1,25 @@
+public class MyFirstGame {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Я загадал число от 1 до 100, твоя задача - отгадать его!");
+        System.out.println("Удачи!");
+        System.out.println("Введите свое число.");
+        
+        int guessNumber = 45;
+        int playerNumber;
+
+        do {
+            playerNumber = scan.nextInt();
+            if (playerNumber < guessNumber) {
+                System.out.println("Введеное мной число - больше!");
+            } else if (playerNumber > guessNumber) {
+                System.out.println("Введеное мной число - меньше!");
+            } else {
+                break;
+            }
+        } while (playerNumber != guessNumber);
+               
+        System.out.println("Поздравляю, ты угадал!");                         
+    }
+}
